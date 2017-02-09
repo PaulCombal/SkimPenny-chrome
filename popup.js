ec5c9e49fce7bdc445db348af77baa471ca67a46
Paul <abonnementspaul@gmail.com>
2017-02-09 21:21:10 +0100

    Improved until site gets better

---
+++ popup.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function() {
+	$.post("http://scroogealpha.esy.es/get.php", 
+	{
+		store : storeName,
+		product : productPage
+	},
+	requestResult,
+	'text')
+	.fail(function(){
+		console.log("Error sending request :(");
+	});
+
+	function requestResult(data){
+
+	}
+});*/
--- scrooge.js
+++ scrooge.js
@@ -10,9 +10,27 @@ function sendToDB(storeName, productPage, price){
 	console.log(storeName);
 	console.log(productPage);
 	console.log(price);
+
+	$.post("http://scroogealpha.esy.es/add.php", 
+		{
+			store : storeName,
+			product : productPage,
+			price : price
+		},
+		requestResult,
+		'text')
+	.fail(function(){
+		console.log("Error sending request :(");
+	});
+
+}
+
+function requestResult(html){
+	console.log("Scrooge answered with:");
+	console.log(html);
 }
 
 function processLDLC(){
 	var price = $(document).find("#productshipping meta[itemprop=price]").attr("content").replace(/,/g, '.');
-	sendToDB("LDLC", window.location.href, price);
+	sendToDB("LDLC", window.location.pathname /*+ window.location.search*/, price);
 }
